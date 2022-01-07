69eed0ca7e7f69b5c91b989eb8dc7c167b028d74
Ervin <dystoppai@gmail.com>
2022-01-07 15:34:36 +0800

    Add quacking duck image

--- clicker.js
+++ clicker.js
@@ -1,9 +1,20 @@
 import React, { useState } from "react";
 import { StyleSheet, Text, TouchableOpacity, View, Image, TouchableWithoutFeedback } from "react-native";
+import { Component } from "react/cjs/react.production.min";
 
 const Clicker = () => {
   const [count, setCount] = useState(0);
   const onPress = () => setCount(prevCount => prevCount + 1);
+
+  const [showDuckQuack, setDuckState] = useState(false);
+  const changeDuck = () => {
+    setDuckState(showDuckQuack ? false : true);
+    // showDuckQuack ? defaultDuck : defaultDuckQuack;
+  }
+  const defaultDuck = require("./assets/duck_default.png");
+  const defaultDuckQuack = require("./assets/duck_default_quack.png");
+
+  var imgSource = showDuckQuack ? defaultDuckQuack : defaultDuck;
 
   return (
     <View style={styles.container}>
@@ -13,8 +24,10 @@ const Clicker = () => {
       <TouchableWithoutFeedback
         style={styles.button}
         onPress={onPress}
+        onPressIn={changeDuck}
+        onPressOut={changeDuck}
       >
-        <Image source={require('./assets/duck_default.png')}/>
+        <Image source={imgSource}/>
       </TouchableWithoutFeedback>
     </View>
   );
